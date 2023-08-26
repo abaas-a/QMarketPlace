8a20e9f312c22fdd6855c8161cde255760c74211
abaas.ahmed <abaas.ahmed@oulook.com>
2023-08-26 13:59:49 +0100

    Add: alt name for image

--- src/components/TutorAds.js
+++ src/components/TutorAds.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TutorAds( { imageSrc, name, description, experience, price }) {
   return (
     <div className='Tutor-Ads'>
-        <img src={imageSrc} className="App-logo" />
+        <img src={imageSrc} alt={name} className="App-logo" />
         <div className="add-content">
             <h3 className="app-header">{name}</h3>
             <p className="add-description">{description}</p>
